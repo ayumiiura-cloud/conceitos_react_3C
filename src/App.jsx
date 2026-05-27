434ba2478e551d5d817aa637c4ca8440c6e89ec2
AYUMI IURA <ayumi_iura@estudante.sesisenai.org.br>
2026-05-27 08:59:44 -0300

    gay

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Cadastro from './paginas/Cadastro'
 import Login from './paginas/Login'
-import Usuarios from './paginas/Usuarios'
 
 function App() {
   const [tela, setTela] = useState('login')
@@ -13,13 +12,14 @@ function App() {
 
   const renderizar = () => {
     if (tela === 'usuarios') {
-      return <Usuarios/>
-    } else if (tela === 'login') {
-      return <Login/>
-    } else if(tela === 'cadastro') {
-      return <Cadastro/>
-    } else{
-      return <Login/>
+      return <Usuarios navegar={trocarDeTela}/>
+    }
+     else if (tela === 'login') {
+      return <Login navegar={trocarDeTela}/>
+    } else if (tela === 'cadastro') {
+      return <Cadastro navegar={trocarDeTela}/>
+    } else {
+      return <Login navegar={trocarDeTela}/>
     }
   }
 
@@ -27,7 +27,7 @@ function App() {
     <>
       <button onClick={() => trocarDeTela('cadastro')}>Cadastro</button>
       <button onClick={() => trocarDeTela('login')}>Login</button>
-      <button onClick={() => trocarDeTela('usuarios')}>Usuarios</button>
+      
       <hr />
       {renderizar()}
     </>
--- src/paginas/login.jsx
+++ src/paginas/login.jsx
@@ -1,6 +1,38 @@
-export default function Login(){
+import { useState } from "react"
 
-    return (
-        <h1>LOGIN</h1>
+export default function Login({navegar}){
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const entrar = async () => {
+        const resultado = await fetch('http://localhost:3000/entrar', {
+            method: 'POST',
+            headers: {'constent-type': 'application/json'},
+            body: JSON.stringify({email, senha})
+        })
+        const data = await resultado.json()
+        console.log(data);
+        console.log(resultado.ok);
+        if (!resultado.ok) {
+            alert(data.error)
+        } else {
+            navegar('usuarios')
+        }
+    }
+    
+    return(
+        <div>
+            <input type="text" id="email"
+            placeholder="digite seu email"
+            value={email}
+            onchange={e => setEmail(e.target.value)}
+             />
+            <input type="password" id="senha"
+            placeholder="digite sua senha"
+            value={senha}
+            onchange={e => setSenha(e.target.value)}
+             />
+            <button onClick={entrar}>Entrar</button>
+        </div>
     )
 }
